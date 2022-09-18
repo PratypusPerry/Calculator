9206f531884cae0f0207500a2b45c70b0e995ae2
Pratyush <pratyushbanerjeeadv6@gmail.com>
2022-09-18 21:41:28 +0530

    calculator added, Manually Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
-import New from './any/New';
-import Chew from './any/Chew';
+import './any/Calcu.css'
+// import './App.css';
+// import New from './any/New';
+// import Chew from './any/Chew';
+import Calcu from './any/Calcu';
 
 const App=()=> {
   return(<>
-    <New/>
-  <Chew/>
+  <Calcu/>
   </>)
 }
 
